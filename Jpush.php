fd2551bd1930a7d9bb0cc5ac99f37a2e8cc60cd1
liyifei2019 <119422342@qq.com>
2019-06-10 21:52:31 +0800

    修改代码

--- Jpush.php
+++ Jpush.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace lspbupt\Jpush;
+
+use JPush\Client;
 use \Yii;
 use \yii\base\Component;
 use \yii\base\InvalidConfigException;
@@ -19,12 +22,12 @@ class Jpush extends Component
     public function init()
     {
         parent::init();
-        if(empty($this->app_key) || empty($this->app_secret)) {
-            throw new InvalidConfigException("app_key和app_secret必须设置"); 
+        if (empty($this->app_key) || empty($this->app_secret)) {
+            throw new InvalidConfigException("app_key和app_secret必须设置");
         }
-        $this->jpush = new \JPush($this->app_key, $this->app_secret);
+        $this->jpush = new Client($this->app_key, $this->app_secret);
     }
-    
+
     public function __call($method, $args = [])
     {
         return call_user_func_array(array($this->jpush, $method), $args);
